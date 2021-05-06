2a3c23818af82df8100242248d988bd7eced50c6
OE.Heart <leoouyixin@126.com>
2021-05-06 19:09:32 +0800

    Complete pdshow

--- Assignment4/Personal-Diary/pdshow.cpp
+++ Assignment4/Personal-Diary/pdshow.cpp
@@ -2,9 +2,44 @@
  * @Author: Ou Yixin
  * @Date: 2021-05-04 20:24:23
  * @LastEditors: Ou Yixin
- * @LastEditTime: 2021-05-04 20:39:13
+ * @LastEditTime: 2021-05-06 19:07:42
  * @Description: 
  * @FilePath: /Personal-Diary/pdshow.cpp
  */
 
 #include "Entity.hpp"
+
+int main(int argc, char *argv[])
+{
+    vector<Entity> diary;
+    loadEntity(&diary);
+
+    string date_buf = argv[1];
+
+    bool tag = false;
+    vector<Entity>::size_type i;
+    for (i = 0; i < diary.size(); i++)
+    {
+        if (diary[i].getDate() == date_buf)
+        {
+            tag = !tag;
+            break;
+        }
+    }
+
+    if (tag)
+    {
+        for (vector<string>::size_type j = 0; j < diary[i].getContentSize(); j++)
+        {
+            cout << diary[i].getContent(j) << endl;
+        }
+    }
+    else
+    {
+        cout << "Diary at date " << date_buf << " doesn't exist.\n";
+        return -1;
+    }
+
+    updateEntity(&diary);
+    return 0;
+}
